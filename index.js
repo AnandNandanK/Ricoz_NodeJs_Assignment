053098517fdcdf300b9a304d509dfaee86fb718c
AnandNandanK <anandwising@gmail.com>
2024-10-07 10:52:33 +0530

    adding default route

--- index.js
+++ index.js
@@ -10,9 +10,14 @@ app.use(express.json()); // for parsing frontend json string data
 app.use(cookieParser());// using cookiParser middelware
 
 
+// Default route
+app.get("/", (req, res) => {
+    res.send("Server is up and running");
+});
+
+
 //using all user apis here
 app.use("/api/v1/user",userRoute)
-
 
 //setting up port 
 const PORT = process.env.PORT || 3000;
